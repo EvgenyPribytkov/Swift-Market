db00630b16cb3b73c8445dd473ee6065696f58ae
EvgenyPribytkov <evgenyprib@gmail.com>
2023-04-08 23:46:47 +0200

    Project setup

---
+++ src/products.js
@@ -0,0 +1,59 @@
+import product1 from "./assets/products/1.jpg";
+import product2 from "./assets/products/2.jpg";
+import product3 from "./assets/products/3.jpg";
+import product4 from "./assets/products/4.jpg";
+import product5 from "./assets/products/5.jpg";
+import product6 from "./assets/products/6.jpg";
+import product7 from "./assets/products/7.webp";
+import product8 from "./assets/products/8.jpg";
+
+export const PRODUCTS = [
+  {
+    id: 1,
+    productName: "AirTag",
+    price: 100.0,
+    productImage: product1,
+  },
+  {
+    id: 2,
+    productName: "Sleep Eye Mask",
+    price: 15.0,
+    productImage: product2,
+  },
+  {
+    id: 3,
+    productName: "Travel Pillow",
+    price: 25.0,
+    productImage: product3,
+  },
+  {
+    id: 4,
+    productName: "Bike Phone Mount",
+    price: 40.0,
+    productImage: product4,
+  },
+  {
+    id: 5,
+    productName: "Packing Cubes for Travel",
+    price: 13.0,
+    productImage: product5,
+  },
+  {
+    id: 6,
+    productName: "Fjällräven Backpack",
+    price: 110.0,
+    productImage: product6,
+  },
+  {
+    id: 7,
+    productName: "Le Creuset Stoneware U Mug",
+    price: 30.0,
+    productImage: product7,
+  },
+  {
+    id: 8,
+    productName: "12 oz Travel Mug",
+    price: 20.0,
+    productImage: product8,
+  },
+];
